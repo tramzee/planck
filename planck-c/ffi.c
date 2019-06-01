ca57325dcb2d91471180d0860bf3db6d57dbf089
Mike Fikes <mike@fikesfarm.com>
2019-06-01 18:15:16 -0400

    wip on breaking apart call

--- planck-c/ffi.c
+++ planck-c/ffi.c
@@ -381,6 +381,300 @@ JSValueRef function_native_call(JSContextRef ctx, JSObjectRef function, JSObject
     return JSValueMakeNull(ctx);
 }
 
+typedef struct native_info {
+    ffi_cif* cifp;
+    size_t arg_count;
+    int* type_ints;
+    int return_type_int;
+    void **arg_values;
+    void* result;
+} native_info_t;
+
+native_info_t native_infos[1024];
+
+JSValueRef function_invoke_native(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+                                  size_t argc, const JSValueRef args[], JSValueRef *exception) {
+
+    native_info_t *native_info = &native_infos[0];
+
+    JSObjectRef arg_values_ref = JSValueToObject(ctx, args[1], NULL);
+
+    size_t arg_count = native_info->arg_count;
+    void **arg_values = native_info->arg_values;
+    int *type_ints = native_info->type_ints;
+
+    size_t i;
+    for (i = 0; i < arg_count; i++) {
+        // TODO maybe just get this directly from args somehow
+        JSValueRef arg_value = JSObjectGetPropertyAtIndex(ctx, arg_values_ref, i, NULL);
+        switch (type_ints[i]) {
+            case FFI_TYPE_UINT8:
+                *(unsigned char *) arg_values[i] = (unsigned char) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_SINT8:
+                *(signed char *) arg_values[i] = (signed char) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_UINT16:
+                *(unsigned short *) arg_values[i] = (unsigned short) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_SINT16:
+                *(short *) arg_values[i] = (short) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_UINT32:
+                *(portable_uint32_t *) arg_values[i] = (portable_uint32_t) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_SINT32:
+                *(portable_int32_t *) arg_values[i] = (portable_int32_t) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_UINT64:
+                *(signed long long *) arg_values[i] = (signed long long) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_SINT64:
+                *(long long *) arg_values[i] = (long long) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_FLOAT:
+                *(float *) arg_values[i] = (float) JSValueToNumber(ctx, arg_value, NULL);
+                break;
+            case FFI_TYPE_DOUBLE:
+                *(double *) arg_values[i] = JSValueToNumber(ctx, arg_value, NULL);
+                break;
+#ifdef FFI_TYPE_LONGDOUBLE
+            case FFI_TYPE_LONGDOUBLE:
+                *(long double *) arg_values[i] = JSValueToNumber(ctx, arg_value, NULL);
+                break;
+#endif
+            default: {
+                JSValueRef arguments[1];
+                arguments[0] = c_string_to_value(ctx, "unhandled type ints case");
+                *exception = JSObjectMakeError(ctx, 1, arguments, NULL);
+                return JSValueMakeNull(ctx);
+            }
+                break;
+        }
+    }
+
+    void* result = native_info->result;
+    ffi_call(native_info->cifp, fp, result, arg_values);
+
+    int return_type_int = native_info->return_type_int;
+
+    // Marshal the return value
+
+    JSValueRef rv = NULL;
+    double temp;
+    switch (return_type_int) {
+        case FFI_TYPE_UINT8:
+            temp = (double) *(unsigned char *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_SINT8:
+            temp = (double) *(signed char *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_UINT16:
+            temp = (double) *(unsigned short *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_SINT16:
+            temp = (double) *(short *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_UINT32:
+            temp = (double) *(portable_uint32_t *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_SINT32:
+            temp = (double) *(portable_int32_t *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_UINT64:
+            temp = (double) *(long long *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_SINT64:
+            temp = (double) *(unsigned long long *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+        case FFI_TYPE_FLOAT:
+            result = malloc(sizeof(float));
+            break;
+        case FFI_TYPE_DOUBLE:
+            rv = JSValueMakeNumber(ctx, *(double *) result);
+            break;
+#ifdef FFI_TYPE_LONGDOUBLE
+        case FFI_TYPE_LONGDOUBLE:
+            temp = (double) *(long double *) result;
+            rv = JSValueMakeNumber(ctx, temp);
+            break;
+#endif
+        default: {
+            JSValueRef arguments[1];
+            arguments[0] = c_string_to_value(ctx, "unhandled result marshal type ints case");
+            *exception = JSObjectMakeError(ctx, 1, arguments, NULL);
+            return JSValueMakeNull(ctx);
+        }
+            break;
+    }
+
+    return rv;
+}
+
+
+JSValueRef function_register_native(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+                                    size_t argc, const JSValueRef args[], JSValueRef *exception) {
+    if (argc > 0
+        && JSValueGetType(ctx, args[0]) == kJSTypeString // Encoded fp
+        && JSValueGetType(ctx, args[1]) == kJSTypeNumber // return type
+        && JSValueGetType(ctx, args[2]) == kJSTypeObject // array of numbers, arg types
+        ) {
+
+        char *fp_str = value_to_c_string(ctx, args[0]);
+        void *fp = str_to_void_star(fp_str);
+        free(fp_str);
+
+        unsigned int arg_count = 1; // TODO, derive from args[2] length
+
+        JSObjectRef arg_types_ref = JSValueToObject(ctx, args[2], NULL);
+
+        int type_ints[arg_count];
+        unsigned int i;
+        for (i = 0; i < arg_count; i++) {
+            type_ints[i] = (int) JSValueToNumber(ctx, JSObjectGetPropertyAtIndex(ctx, arg_types_ref, i, NULL), NULL);
+        }
+
+        ffi_cif cif;
+        ffi_type *arg_types[arg_count];
+
+        for (i = 0; i < arg_count; i++) {
+            arg_types[i] = int_to_ffi_type(type_ints[i]);
+        }
+
+        int return_type_int = (int) JSValueToNumber(ctx, args[1], NULL);
+        ffi_type *return_type = int_to_ffi_type(return_type_int);
+
+        ffi_status status;
+
+        // Prepare the ffi_cif structure.
+        if ((status = ffi_prep_cif(&cif, FFI_DEFAULT_ABI,
+                                   arg_count, return_type, arg_types)) != FFI_OK) {
+            JSValueRef arguments[1];
+            char* errmsg = NULL;
+            if (status == FFI_BAD_ABI) {
+                errmsg = "ffi_prep_cif failed returning FFI_BAD_ABI";
+            } else if (status == FFI_BAD_TYPEDEF) {
+                errmsg = "ffi_prep_cif failed returning FFI_BAD_TYPEDEF";
+            } else {
+                errmsg = "ffi_prep_cif failed";
+            }
+            arguments[0] = c_string_to_value(ctx, errmsg);
+            *exception = JSObjectMakeError(ctx, 1, arguments, NULL);
+            return JSValueMakeNull(ctx);
+        }
+
+        JSObjectRef arg_values_ref = JSValueToObject(ctx, args[3], NULL);
+
+        void *arg_values[arg_count];
+        for (i = 0; i < arg_count; i++) {
+            JSValueRef arg_value = JSObjectGetPropertyAtIndex(ctx, arg_values_ref, i, NULL);
+            switch (type_ints[i]) {
+                case FFI_TYPE_UINT8:
+                    arg_values[i] = malloc(1);
+                    break;
+                case FFI_TYPE_SINT8:
+                    arg_values[i] = malloc(1);
+                    break;
+                case FFI_TYPE_UINT16:
+                    arg_values[i] = malloc(2);
+                    break;
+                case FFI_TYPE_SINT16:
+                    arg_values[i] = malloc(2);
+                    break;
+                case FFI_TYPE_UINT32:
+                    arg_values[i] = malloc(4);
+                    break;
+                case FFI_TYPE_SINT32:
+                    arg_values[i] = malloc(4);
+                    break;
+                case FFI_TYPE_UINT64:
+                    arg_values[i] = malloc(8);
+                    break;
+                case FFI_TYPE_SINT64:
+                    arg_values[i] = malloc(8);
+                    break;
+                case FFI_TYPE_FLOAT:
+                    arg_values[i] = malloc(sizeof(float));
+                    break;
+                case FFI_TYPE_DOUBLE:
+                    arg_values[i] = malloc(sizeof(double));
+                    break;
+#ifdef FFI_TYPE_LONGDOUBLE
+                case FFI_TYPE_LONGDOUBLE:
+                    arg_values[i] = malloc(sizeof(long double));
+                    break;
+#endif
+                default: {
+                    JSValueRef arguments[1];
+                    arguments[0] = c_string_to_value(ctx, "unhandled type ints case");
+                    *exception = JSObjectMakeError(ctx, 1, arguments, NULL);
+                    return JSValueMakeNull(ctx);
+                }
+                    break;
+            }
+        }
+
+        // Allocate space to hold the return value
+
+        void *result = NULL;
+        switch (return_type_int) {
+            case FFI_TYPE_UINT8:
+                result = malloc(1);
+                break;
+            case FFI_TYPE_SINT8:
+                result = malloc(1);
+                break;
+            case FFI_TYPE_UINT16:
+                result = malloc(2);
+                break;
+            case FFI_TYPE_SINT16:
+                result = malloc(2);
+                break;
+            case FFI_TYPE_UINT32:
+                result = malloc(4);
+                break;
+            case FFI_TYPE_SINT32:
+                result = malloc(4);
+                break;
+            case FFI_TYPE_UINT64:
+                result = malloc(8);
+                break;
+            case FFI_TYPE_SINT64:
+                result = malloc(8);
+                break;
+            case FFI_TYPE_FLOAT:
+                result = malloc(sizeof(float));
+                break;
+            case FFI_TYPE_DOUBLE:
+                result = malloc(sizeof(double));
+                break;
+#ifdef FFI_TYPE_LONGDOUBLE
+            case FFI_TYPE_LONGDOUBLE:
+                result = malloc(sizeof(long double));
+                break;
+#endif
+            default: {
+                JSValueRef arguments[1];
+                arguments[0] = c_string_to_value(ctx, "unhandled result type ints case");
+                *exception = JSObjectMakeError(ctx, 1, arguments, NULL);
+                return JSValueMakeNull(ctx);
+            }
+                break;
+        }
+
+    }
+
+    return JSValueMakeNull(ctx);
+}
+
 JSValueRef function_uname(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
                           size_t argc, const JSValueRef args[], JSValueRef *exception) {
 
